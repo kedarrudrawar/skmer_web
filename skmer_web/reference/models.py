65d2cb9894ede7e1f2d5a27cbea16f83f17e2857
Kedar Rudrawar <rudrawarkedar@gmail.com>
2019-05-25 16:39:11 -0700

    add templates and alter models

--- skmer_web/reference/models.py
+++ skmer_web/reference/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class RefLibrary(models.Model):
-    reference_name = models.CharField(max_length=50)
+class RefFile(models.Model):
+    reference_name = models.TextField(max_length=50)
     file = models.FileField(upload_to='reference_lib/')
--- skmer_web/reference/views.py
+++ skmer_web/reference/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Home Page.')
+def home_view(request, *args, **kwargs):
+    print(request)
+    print(request.user)
+    # return HttpResponse("<h1> Home page. </h1>") 
+    return render(request, "home.html", {})
