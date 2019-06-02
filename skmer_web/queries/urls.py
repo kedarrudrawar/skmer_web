cb8bc97a01858478891aef609539c993fb238b92
Kedar Rudrawar <rudrawarkedar@gmail.com>
2019-06-02 13:50:19 -0700

    add static files

--- skmer_web/queries/urls.py
+++ skmer_web/queries/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 app_name = 'queries'
 urlpatterns = [
-    path('analyze/', views.upload_queryfile, name='input'),
+    path('analyze/single', views.upload_queryfile, name='input_single'),
+    path('analyze/multiple', views.upload_queryfile, name='input_multiple'),
     path('query_list/', views.query_list, name='query_list'),
     path(r'query_list/query_id=<int:query_id>', views.analyze_file, name='analyze')
 ]
--- skmer_web/queries/views.py
+++ skmer_web/queries/views.py
@@ -5,23 +5,16 @@ from django.views import generic
 from urllib.parse import urlencode
 
 import os,sys,inspect
-sys.path.append('/Users/KedarRudrawar/Desktop/Spring_Quarter_2019/CSE-182/skmer_web/skmer_web/')
+# sys.path.append('/Users/KedarRudrawar/Desktop/Spring_Quarter_2019/CSE-182/skmer_web/skmer_web/')
+sys.path.append(settings.BASE_DIR)
 
 from .forms import QueryForm, RawQueryForm
 from .models import Query
 from scripts.skmer_functions import parse_queryout, query
 import time
-BASE_DIR = '/Users/KedarRudrawar/Desktop/Spring_Quarter_2019/CSE-182/skmer_web/skmer_web/'
-STATIC_DIR = BASE_DIR + 'static/'
-REF_DIR_PATH = STATIC_DIR + 'ref_dir/'
 
-
-
-class DetailView(generic.DetailView):
-    model = Query
-    template_name = 'queries/detail.html'
-    def get_queryset(self):
-        return render(Query.objects.all())
+STATIC_DIR = os.path.join(settings.BASE_DIR, 'static')
+REF_DIR_PATH = os.path.join(STATIC_DIR, 'ref_dir')
 
 
 def analyze_file(request, query_id):
@@ -32,13 +25,9 @@ def analyze_file(request, query_id):
         return render(request, 'queries/blank.html')
 
     else:
-        print('FILE PATH', query_file)
-        library_dir = STATIC_DIR + 'testlib'
-        output_prefix = BASE_DIR + r'media/skmer_output/output/'
+        library_dir = os.path.join(STATIC_DIR, 'testlib')
+        output_prefix = os.path.join(settings.BASE_DIR, 'media/skmer_output/output')
         out = query(query_file, library_dir, output_prefix, add_query_to_ref=False)
-        time.sleep(5)
-        print("Output file exists?", os.path.isfile(out))
-        print('OUTPUT', out)
         list_of_hit_distance_pairs = parse_queryout(out)
 
         print(list_of_hit_distance_pairs)
@@ -49,37 +38,6 @@ def analyze_file(request, query_id):
         }
 
         return render(request, 'queries/analysis.html', context)
-
-
-# def query_create_view(request):
-#     form = RawQueryForm()
-#     if request.method == 'POST':
-#         form = RawQueryForm(request.POST)
-#         if form.is_valid():
-#             print(form.cleaned_data)
-#             Query.objects.create(**form.cleaned_data)
-#         else:
-#             print(form.errors)
-#
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'queries/query_create.html', context)
-
-
-# def upload_file(request):
-#     if request.method == 'POST':
-#         file = request.FILES.get('Query file')
-#         storage = FileSystemStorage()
-#         storage.save(file.name, file)
-#         file_path = '\"' + os.path.join(settings.MEDIA_ROOT, file.name) + '\"'
-#         print(file_path)
-#
-#         command = 'python3 scripts/test.py -f {}'.format(file_path)
-#         output = os.popen(command).read()
-#         print(output)
-#
-#     return render(request, 'queries/query_create.html')
 
 
 def query_list(request):
@@ -113,6 +71,72 @@ def upload_queryfile(request):
     }
 
     return render(request, 'queries/query_create.html', context)
+
+
+def upload_queryfile_multiple(request):
+    if request.method == 'POST':
+        form = QueryForm(request.POST, request.FILES)
+        if form.is_valid():
+            q = form.save()
+            query_string = urlencode({'query_id': q.id})
+
+            base_url = reverse('queries:query_list')
+            url = '%s%s' % (base_url, query_string)
+            return redirect(url)
+    else:
+        form = QueryForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'queries/query_create.html', context)
+
+
+
+
+
+# class DetailView(generic.DetailView):
+#     model = Query
+#     template_name = 'queries/detail.html'
+#     def get_queryset(self):
+#         return render(Query.objects.all())
+
+
+
+
+# def query_create_view(request):
+#     form = RawQueryForm()
+#     if request.method == 'POST':
+#         form = RawQueryForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             Query.objects.create(**form.cleaned_data)
+#         else:
+#             print(form.errors)
+#
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'queries/query_create.html', context)
+
+
+# def upload_file(request):
+#     if request.method == 'POST':
+#         file = request.FILES.get('Query file')
+#         storage = FileSystemStorage()
+#         storage.save(file.name, file)
+#         file_path = '\"' + os.path.join(settings.MEDIA_ROOT, file.name) + '\"'
+#         print(file_path)
+#
+#         command = 'python3 scripts/test.py -f {}'.format(file_path)
+#         output = os.popen(command).read()
+#         print(output)
+#
+#     return render(request, 'queries/query_create.html')
+
+
+
 
 
 
